c166479146f1de85bac8da2e3e38a09a49b8620d
tien.nguyenvinh <tien.nguyenvinh@di4l.vn>
2024-05-22 17:20:38 +0700

    new api

--- lib/common/api_url.dart
+++ lib/common/api_url.dart
@@ -1,12 +1,11 @@
 import 'dart:io';
 
 class ApiURL {
-  static const apiUrl = "https://4135-125-235-239-15.ngrok-free.app/api";
+  static const apiUrl = "https://e09c-125-235-239-15.ngrok-free.app/api";
   static const headers = {HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8'};
   static const login = "$apiUrl/login";
   static const register = "$apiUrl/register";
   static const getUserFriends = "$apiUrl/get-user-friends";
-  static const getUserConversations = "$apiUrl/get-user-conversations";
   static const message = "$apiUrl/message";
   static const deleteMessage = "$apiUrl/delete-message";
   static const updateUserAvatar = "$apiUrl/update-user-avatar";
@@ -15,4 +14,6 @@ class ApiURL {
   static const group = "$apiUrl/group";
   static const addUserGroup = "$apiUrl/group/add-user-group";
   static const conversation = "$apiUrl/conversation";
+  static const checkForConversationWithUser = "$apiUrl/check-for-conversation-with-user";
+  static const getMeeting = "$apiUrl/get-meeting";
 }
